cb5d09ae266d80b55a401f84e92062522f1c7e88
Grant Alexander <grant.m.alexander@hotmail.com>
2019-03-30 23:32:08 -0500

    Correct invalid reference

--- testing/TestResultAggregator.cc
+++ testing/TestResultAggregator.cc
@@ -19,7 +19,7 @@ void TestResultAggregator<S>::DisplayResults(std::array<TestResult, S> results)
     for(int x = 0; x < results.size(); x++)
     {
         TestResult currentResult = results[x];
-        std::string textResult = currentResult->GetStatusString();
+        std::string textResult = currentResult.GetStatusString();
 
         std::cout << "\t" << "Test Case " << x << " - " << currentResult.GetName() << ": " << textResult << " \n";
         std::cout << "\t" << currentResult.GetMessage() << "\n";
