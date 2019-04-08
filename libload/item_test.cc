0713d72bce6d49b43bd524536946f78768f04ed5
Grant Alexander <grant.m.alexander@hotmail.com>
2019-04-07 21:06:14 -0500

    Add two new unit tests

--- libload/item_test.cc
+++ libload/item_test.cc
@@ -1,9 +1,9 @@
 #include "item.cc"
 #include "yautf.hh"
 
-yautf::Test<bool>* case1_verifySubtraction()
+yautf::Test<bool>* case1_verifyCompleteSubtraction()
 {
-    std::string caseName = "Verify Subtraction";
+    std::string caseName = "Verify Complete Subtraction";
     std::string fixtureName = "Fixture";
     int runTime = 100;
     bool expectedValue = true;
@@ -18,10 +18,47 @@ yautf::Test<bool>* case1_verifySubtraction()
     return verifySubtraction;
 }
 
+yautf::Test<bool>* case1_verifyIncompleteSubtraction()
+{
+    std::string caseName = "Verify Incomplete Subtraction";
+    std::string fixtureName = "Fixture";
+    int runTime = 100;
+    bool expectedValue = false;
+    int valueToSubtract = 50;
+    auto verifyIncompleteSubtraction = new yautf::Test<bool>(caseName, expectedValue);
+    Item fixture(fixtureName, runTime);
+    bool actualValue;
+
+    fixture.SubtractTime(valueToSubtract);
+    actualValue = fixture.IsDone();
+    verifyIncompleteSubtraction->SetActual(actualValue);
+    return verifyIncompleteSubtraction;
+}
+
+yautf::Test<bool>* case1_verifyOvertimeSubtraction()
+{
+    std::string caseName = "Verify Overtime Subtraction";
+    std::string fixtureName = "Fixture";
+    int runTime = 100;
+    bool expectedValue = true;
+    int valueToSubtract = 101;
+    auto verifyOvertimeSubtraction = new yautf::Test<bool>(caseName, expectedValue);
+    Item fixture(fixtureName, runTime);
+    bool actualValue;
+
+    fixture.SubtractTime(valueToSubtract);
+    actualValue = fixture.IsDone();
+    verifyOvertimeSubtraction->SetActual(actualValue);
+    return verifyOvertimeSubtraction;
+}
+
+
 int main()
 {
     auto manager = new yautf::TestManager<bool>();
-    manager->LoadTest(case1_verifySubtraction());
+    manager->LoadTest(case1_verifyCompleteSubtraction());
+    manager->LoadTest(case2_verifyIncompleteSubtraction());
+    manager->LoadTest(case3_verifyOvertimeSubtraction());
     manager->VerifyTests();
     manager->DisplayAllResults();
     delete manager;
