d3ce31d0b43f8e75d55df55c5b1459772f2e23fa
Grant Alexander <grant.m.alexander@hotmail.com>
2019-04-10 19:43:46 -0500

    add fourth unit test for executor.cc

--- libload/executor_test.cc
+++ libload/executor_test.cc
@@ -63,12 +63,41 @@ yautf::Test<bool>* case3_verifyFullTimeAppliedOneItem()
     return fullTimeAppliedOneTime;
 }
 
+yautf::Test<bool>* case4_verifyFullTimeAppliedTwoItems()
+{
+    std::string caseName = "Verify Full Time Applied Against Two Items";
+    std::string fixtureName = "Fixture1";
+    std::string secondFixtureName = "Fixture2";
+    std::string thirdFixtureName = "Fixture3";
+    std::string fourthFixtureName = "Fixture4";
+    bool expectedValue = true;
+    int timeToSubtract = 100;
+    int slots = 2;
+    int runTime = 100;
+    auto fullTimeAppliedOneTime = new yautf::Test<bool>(caseName, expectedValue);
+
+    Item fixture(fixtureName, runTime);
+    Item fixture2(secondFixtureName, runTime);
+    Item fixture3(thirdFixtureName, runTime);
+    Item fixture4(fourthFixtureName, runTime);
+    Executor sys(slots);
+    sys.PushItem(fixture);
+    sys.PushItem(fixture2);
+    sys.ApplyTime(timeToSubtract);
+    bool wasSuccessful = sys.PushItem(fixture3) && sys.PushItem(fixture4);
+    bool actualValue = wasSuccessful;
+
+    fullTimeAppliedOneTime->SetActual(actualValue);
+    return fullTimeAppliedOneTime;
+}
+
 int main()
 {
     auto manager = new yautf::TestManager<bool>();
     manager->LoadTest(case1_verifyItemPushOnNew());
     manager->LoadTest(case2_verifyItemRejectedOnFull());
     manager->LoadTest(case3_verifyFullTimeAppliedOneItem());
+    manager->LoadTest(case4_verifyFullTimeAppliedTwoItems());
     manager->VerifyTests();
     manager->DisplayAllResults();
     delete manager;
