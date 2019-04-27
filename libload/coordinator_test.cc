f454a7b0242fed2a80e9161d4ef605df822aa504
Grant Alexander <grant@grantsweb.site>
2019-04-26 21:23:55 -0500

    Commit new failing unit test

--- libload/coordinator_test.cc
+++ libload/coordinator_test.cc
@@ -21,10 +21,35 @@ yautf::Test<int>* case1_verifyTimeForOneExecutorOneSlotOneItem()
     return verifyTimeForOneExecutorOneSlotOneItem;
 }
 
+yautf::Test<int>* case2_verifyTimeForOneExecutorOneSlotTwoItems()
+{
+    std::string caseName = "Verify Proper Time for One Executor with One Slot and Two Items";
+    int expectedValue = 250;
+    auto verifyTimeForOneExecutorOneSlotOneItem = new yautf::Test<int>(caseName, expectedValue);
+    std::string fixtureName = "Fixture 1";
+    std::string fixture2Name = "Fixture 2";
+    int runTime = 100;
+    int runTime2 = 150;
+    int slots = 1;
+    Item fixture(&fixtureName, runTime);
+    Item fixture2(&fixture2Name, runTime2);
+    Executor exec(slots);
+    auto items = new std::vector<Item>();
+    items->push_back(fixture);
+    items->push_back(fixture2);
+    auto executors = new std::vector<Executor>();
+    executors->push_back(exec);
+    Coordinator coord(executors, items);
+    int actualValue = coord.Calculate();
+    verifyTimeForOneExecutorOneSlotOneItem->SetActual(actualValue);
+    return verifyTimeForOneExecutorOneSlotOneItem;
+}
+
 int main()
 {
     auto mgmt = new yautf::TestManager<int>();
     mgmt->LoadTest(case1_verifyTimeForOneExecutorOneSlotOneItem());
+    mgmt->LoadTest(case2_verifyTimeForOneExecutorOneSlotTwoItems());
     mgmt->VerifyTests();
     mgmt->DisplayAllResults();
 }
