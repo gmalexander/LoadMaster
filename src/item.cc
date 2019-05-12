7ad20f65d6a9b123c6199d0e1c91b658792a47ef
gmalexander <grant.m.alexander@hotmail.com>
2019-05-12 11:04:45 -0500

    Update license to LGPL v.3 (it was implicitly GPL before due to YAUTF's license)

--- src/item.cc
+++ src/item.cc
@@ -1,5 +1,23 @@
 #include "item.hh"
 
+/**************************************************************************************************
+*    LoadMaster, a framework for calculating the duration of clustered tasks                      *
+*    Copyright (C) 2019  Grant Alexander                                                          *
+*                                                                                                 *
+*    This program is free software: you can redistribute it and/or modify                         *
+*    it under the terms of the GNU Lesser General Public License as published by                  *
+*    the Free Software Foundation, either version 3 of the License, or                            *
+*    (at your option) any later version.                                                          *
+*                                                                                                 *
+*    This program is distributed in the hope that it will be useful,                              *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of                               *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                *
+*    GNU General Public License for more details.                                                 *
+*                                                                                                 *
+*    You should have received a copy of the GNU General Public License                            *
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.                       *
+***************************************************************************************************/
+
 Item::Item(std::string* name, int runtime) {
      this->RunTime = runtime;
      this->Name = *name;
